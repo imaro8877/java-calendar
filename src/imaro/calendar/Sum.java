d873acf958e4fb44f8eea098b4b4159ce30bd1bd
USER <USER@USER-PC>
2018-05-30 16:54:25 +0900

    max says of the month

--- src/imaro/calendar/Sum.java
+++ src/imaro/calendar/Sum.java
@@ -15,8 +15,15 @@ public class Sum {
 		b = Integer.parseInt(s2);
 		System.out.println(a + "," + b);
 		System.out.println(a + b);
-		System.out.printf("%d 와 %d의 합은 %d 입니다.", a, b, a + b);
+		System.out.printf("%d 와 %d의 합은 %d 입니다.\n", a, b, a + b);
 		scanner.close();
+
+		System.out.println("월을 입력해주세요.");
+		Scanner scanner1 = new Scanner(System.in);
+		int month = scanner1.nextInt();
+		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+		scanner1.close();
 	}
 
 }
