e436d6075235a3ad8d1dddd509ff71ff94d46f8b
USER <USER@USER-PC>
2018-05-30 15:53:48 +0900

    Change UTF-8

--- src/imaro/calendar/Calendar.java
+++ src/imaro/calendar/Calendar.java
@@ -1,15 +1,14 @@
 package imaro.calendar;
 
 public class Calendar {
-	public static void main(String [] argc)
-	{
-		System.out.println(" ÀÏ  ¿ù   È­   ¼ö   ¸ñ   ±İ   Åä");
+	public static void main(String[] argc) {
+		System.out.println(" ì¼  ì›”   í™”   ìˆ˜   ëª©   ê¸ˆ   í† "); 
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println("29 30 31");
-		
+
 	}
 }
