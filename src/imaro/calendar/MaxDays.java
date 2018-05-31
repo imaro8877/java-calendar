b8a753d87f333b85a9db7f1129ee851ce6f609b9
USER <USER@USER-PC>
2018-05-31 15:38:29 +0900

    MaxdaysofMonth

--- src/imaro/calendar/MaxDays.java
+++ src/imaro/calendar/MaxDays.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class MaxDays {
 
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int maxDaysofMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
 	public static void main(String[] args) {
 		System.out.println("월을 입력해주세요.");
 		Scanner scanner = new Scanner(System.in);
+		MaxDays cal = new MaxDays();
 		int month = scanner.nextInt();
-		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysofMonth(month));
 		scanner.close();
 	}
 
